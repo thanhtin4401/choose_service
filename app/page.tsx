75bfed8d15039341210b1f718ffe3b92e8ed1f76
thanhtin4401 <thanhtin4401.ufm@gmail.com>
2023-10-18 23:47:00 +0700

    complete project

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import ChooseService from "@/components/ChooseService";
-import Image from "next/image";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <main className="">
-      <ChooseService />
+    <main className="flex flex-col items-center justify-between pt-[88px] bg-background-color">
+      <div className="container mx-auto py-5 px-[1.5rem] lg:px-0">
+        <ChooseService />
+      </div>
+      <Footer />
     </main>
   );
 }
--- components/ChooseService.tsx
+++ components/ChooseService.tsx
@@ -2,108 +2,118 @@ import React from "react";
 
 const ChooseService = () => {
   return (
-    <div className="font-Poppins bg-[#F8FAFC] lg:h-[100vh]  p-[50px]">
-      <div className="w-[90%] mx-auto">
-        <h1 className="text-[40px] text-[#0C273C]  font-blod">
+    <div className="font-Poppins bg-[#F8FAFC]">
+      <div className="">
+        <h1 className="text-[24px] lg:text-[40px] mb-0 lg:mb-[65px] text-text-color  font-[700]">
           Choose Service
         </h1>
         <div className="p-[10px]">
-          <div className="bg-white rounded-[10px] flex flex-col p-[50px]">
-            <div className="stepper relative  space-y-5 lg:space-y-0 flex flex-col lg:flex-row justify-between ">
-              <div className="items flex flex-row lg:flex-col  lg:space-y-2 space-x-5 lg:justify-center items-center lg:w-36 ">
-                <svg
-                  className="relative z-10"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="42"
-                  height="42"
-                  viewBox="0 0 42 42"
-                  fill="none"
-                >
-                  <circle cx="21" cy="21" r="21" fill="#EBF0FF" />
-                  <path
-                    d="M11 21L17.6667 28L31 14"
-                    stroke="white"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                </svg>
-                <span className="text-[#0C273C]  font-medium leading-[180%]">
-                  Custumer Detail
-                </span>
+          <div className="bg-white rounded-[10px] flex flex-col px-5 py-5 lg:p-[50px]">
+            <div className="lg:px-0 px-5">
+              <div className="flex flex-col lg:flex-row lg:justify-between lg:items-center items-start space-y-4 lg:space-y-0 relative">
+                <div className=" h-[1px] hidden lg:block bg-primary-color absolute top-[21px] right-0 translate-y-2/4 z-1 w-[90%] mr-[45px]"></div>
+                <div className="flex flex-row lg:flex-col justify-center items-center lg:space-y-2 space-y-0 lg:space-x-0 space-x-4 relative z-10">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="42"
+                    height="42"
+                    viewBox="0 0 42 42"
+                    fill="none"
+                  >
+                    <circle
+                      cx="21"
+                      cy="21"
+                      r="20.5"
+                      fill="#6D6CE3"
+                      stroke="#6D6CE3"
+                    />
+                    <path
+                      d="M11 21L17.6667 28L31 14"
+                      stroke="white"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                  <span className="text-[1rem] font-[500] text-text-color">
+                    Custumer Detail
+                  </span>
+                </div>
+                <div className="flex flex-row lg:flex-col justify-center items-center lg:space-y-2 space-y-0 lg:space-x-0 space-x-4 relative z-10">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="42"
+                    height="42"
+                    viewBox="0 0 42 42"
+                    fill="none"
+                  >
+                    <circle
+                      cx="21"
+                      cy="21"
+                      r="20.5"
+                      fill="#6D6CE3"
+                      stroke="#6D6CE3"
+                    />
+                    <path
+                      d="M11 21L17.6667 28L31 14"
+                      stroke="white"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                  <span className="text-[1rem] font-[500] text-text-color">
+                    Project Details
+                  </span>
+                </div>
+                <div className="flex flex-row lg:flex-col justify-center items-center lg:space-y-2 space-y-0 lg:space-x-0 space-x-4 relative z-10">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="42"
+                    height="42"
+                    viewBox="0 0 42 42"
+                    fill="none"
+                  >
+                    <circle cx="21" cy="21" r="21" fill="#EBF0FF" />
+                    <path
+                      d="M11 21L17.6667 28L31 14"
+                      stroke="white"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                  <span className="text-[1rem] font-[500] text-text-color">
+                    Attached File
+                  </span>
+                </div>
+                <div className="flex flex-row lg:flex-col justify-center items-center lg:space-y-2 space-y-0 lg:space-x-0 space-x-4 relative z-10">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="42"
+                    height="42"
+                    viewBox="0 0 42 42"
+                    fill="none"
+                  >
+                    <circle cx="21" cy="21" r="21" fill="#EBF0FF" />
+                    <path
+                      d="M11 21L17.6667 28L31 14"
+                      stroke="white"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                  <span className="text-[1rem] font-[500] text-text-color">
+                    Payment Details
+                  </span>
+                </div>
               </div>
-              <div className="items flex flex-row lg:flex-col  lg:space-y-2 space-x-5 lg:justify-center items-center lg:w-36 ">
-                <svg
-                  className="relative z-10"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="42"
-                  height="42"
-                  viewBox="0 0 42 42"
-                  fill="none"
-                >
-                  <circle cx="21" cy="21" r="21" fill="#6D6CE3" />
-                  <path
-                    d="M11 21L17.6667 28L31 14"
-                    stroke="white"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                </svg>
-                <span className="text-[#0C273C]  font-medium leading-[180%]">
-                  Project Details
-                </span>
-              </div>
-              <div className="items flex flex-row lg:flex-col  lg:space-y-2 space-x-5 lg:justify-center items-center lg:w-36">
-                <svg
-                  className="relative z-10"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="42"
-                  height="42"
-                  viewBox="0 0 42 42"
-                  fill="none"
-                >
-                  <circle cx="21" cy="21" r="21" fill="#EBF0FF" />
-                  <path
-                    d="M11 21L17.6667 28L31 14"
-                    stroke="white"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                </svg>
-                <span className="text-[#0C273C]  font-medium leading-[180%]">
-                  Attached File
-                </span>
-              </div>
-              <div className="items flex flex-row lg:flex-col  lg:space-y-2 space-x-5 lg:justify-center items-center lg:w-36">
-                <svg
-                  className="relative z-10"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="42"
-                  height="42"
-                  viewBox="0 0 42 42"
-                  fill="none"
-                >
-                  <circle cx="21" cy="21" r="21" fill="#EBF0FF" />
-                  <path
-                    d="M11 21L17.6667 28L31 14"
-                    stroke="white"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                </svg>
-                <span className="text-[#0C273C]  font-medium leading-[180%]">
-                  Payment Details
-                </span>
-              </div>
-              <div className="bg-[#6D6CE3]  absolute  w-[90%] lg:h-[2px] top-[21px] right-[90px]  -z-1"></div>
             </div>
             <div className="mt-[30px]">
               <div className="grid grid-cols-2  gap-x-[100px] rounded-[10px] ">
                 <div className="col-span-2 lg:col-span-1">
-                  <h3 className="text-[20px] font-medium leading-[36px] text-[#0C273C]">
+                  <h3 className="text-[20px] font-medium leading-[36px] text-text-color">
                     Project Name
                   </h3>
                   <input
@@ -112,7 +122,7 @@ const ChooseService = () => {
                   />
                 </div>
                 <div className="col-span-2 lg:col-span-1">
-                  <h3 className="text-[20px] font-medium leading-[36px] text-[#0C273C]">
+                  <h3 className="text-[20px] font-medium leading-[36px] text-text-color">
                     Bussiness Name
                   </h3>
                   <input
@@ -123,7 +133,7 @@ const ChooseService = () => {
               </div>
               <div className="grid grid-cols-1 mt-[20px]">
                 <div>
-                  <h3 className="text-[20px] col-span-2  font-medium leading-[36px] text-[#0C273C]">
+                  <h3 className="text-[20px] col-span-2  font-medium leading-[36px] text-text-color">
                     Project Name
                   </h3>
                   <input
@@ -135,10 +145,10 @@ const ChooseService = () => {
             </div>
             <div className="group-buttons flex justify-end mt-[50px]">
               <div className="">
-                <button className="py-[12px] px-[21px] text-[#6D6CE3] rounded-[28px] border mr-[30px] border-[#6D6CE3]">
+                <button className="py-[12px] px-[21px] text-primary-color rounded-[28px] border mr-[30px] border-primary-color">
                   Back
                 </button>
-                <button className="bg-[#6D6CE3] text-[white] py-[12px] px-[21px] rounded-[28px]">
+                <button className="bg-primary-color text-[white] py-[12px] px-[21px] rounded-[28px]">
                   Next
                 </button>
               </div>
